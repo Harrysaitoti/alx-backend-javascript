2425f89ed857d9db14fa6b2d7743ac1abfcee6ec
Harry Sunkuli <saiotisunkuli@yahoo.com>
2024-03-07 17:53:10 +0000

    3-currency

---
+++ 0x02-ES6_classes/3-main.js
@@ -0,0 +1,8 @@
+import Currency from "./3-currency.js";
+
+const dollar = new Currency('$', 'Dollars');
+console.log(dollar.displayFullCurrency());
+
+bob@dylan:~$ 
+bob@dylan:~$ npm run dev 3-main.js 
+Dollars ($)
